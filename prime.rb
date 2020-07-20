ef73d6b0756ca76be68d4428566404cb0ad67601
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-20 17:45:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,34 @@
-# Add  code here!
+require 'benchmark'
+
+def prime?(num)
+  if num == 2
+    true
+  elsif num > 2 && num.odd?
+    (2..num - 1).to_a.none? do |denom|
+      num % denom == 0
+    end
+  else
+    false
+  end
+end
+
+def is_prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
+end
+
+
+Benchmark.bm do |bm|
+  bm.report do
+    100_000.times do
+      prime?(17)
+    end
+  end
+
+  bm.report do
+    100_000.times do
+      is_prime?(17)
+    end
+  end
+end
